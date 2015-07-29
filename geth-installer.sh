8b71b249963f804bf61e8bb615d5c43f457d5b0b
phonikg <jgerryts@gmail.com>
2015-07-29 11:51:11 -0400

    Create geth-installer.sh

---
+++ geth-installer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Upgrade Debian to "stretch"
+echo -e "\e[32mFirst we need to edit our sources.list to set stretch/testing as our source so we can install latest versions of dependencies\e[0m"
+sudo sed -i 's/vivid/wily/g' /etc/apt/sources.list
+echo -e "\e[34mPerforming Housekeeping - Update...\e[0m"
+sudo apt-get -y update
+echo -e "\e[34mPerforming Housekeeping - Upgrade...\e[0m"
+sudo apt-get -y upgrade
+echo -e "\e[34mPerforming Debian Upgrade from `Jessie` to `Stretch`...\e[0m"
+sudo apt-get -y dist-upgrade 
+
+#Install dependencies
+echo -e "\e[34mInstalling Dependencies...\e[0m"
+sudo apt-get -y install dphys-swapfile build-essential libgmp3-dev golang git python curl
+
+#Install go-ethereum OR `geth`
+echo -e "\e[34mInstalling go-ethereum...\e[0m"
+cd ~
+git clone https://github.com/ethereum/go-ethereum
+cd go-ethereum
+make geth
